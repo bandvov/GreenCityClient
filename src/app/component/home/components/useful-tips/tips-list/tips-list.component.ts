46546166c0c66ce9ef173181d337e0b4e45546ce
jimholman1199 <jimholman1199@gmail.com>
2020-10-06 00:16:19 +0300

    [BUGFIX] Added extra el render

--- src/app/component/home/components/useful-tips/tips-list/tips-list.component.ts
+++ src/app/component/home/components/useful-tips/tips-list/tips-list.component.ts
@@ -1,5 +1,5 @@
 import { TranslateService } from '@ngx-translate/core';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 // tslint:disable-next-line: max-line-length
 import { SwiperDirective, SwiperComponent, SwiperScrollbarInterface, SwiperPaginationInterface, SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
@@ -17,27 +17,7 @@ export class TipsListComponent implements OnInit {
     'assets/img/icon/water-bottle.png',
   ];
   tips;
-
-  // Note: below you can find an old approach
-
-  // tips = [
-  //   {
-  //     imageUrl: 'assets/img/icon/spoon-knife.png',
-  //     text: 'Не ходіть до магазину голодними. Складайте список покупок заздалегідь. Тоди ви витратите менше і не купите зайвого, що потім потрібно буде викидати'
-  //   },
-  //   {
-  //     imageUrl: 'assets/img/icon/water-bottle.png',
-  //     text: 'Найперше і найпростіше − відмовитися від запакованих продуктів. Купуючи овочі чи фрукти, я не беру пакети, а користуюся багаторазовими торбами і судочками для вологих продуктів, таких як м’ясо чи риба.'
-  //   },
-  //   {
-  //     imageUrl: 'assets/img/icon/coffee-cup.png',
-  //     text: 'Слід намагатись збільшувати частку рослинної їжі в раціоні, натомість зменшувати вживання продуктів із консервантами, посилювачами смаку.'
-  //   },
-  //   {
-  //     imageUrl: 'assets/img/icon/water-bottle.png',
-  //     text: 'Посадіть дерево і підтримуйте людей, які зберігають і захищають наші ліси. Дерева поглинають тонну СО2 протягом свого життя, очищуючи повітря'
-  //   }
-  // ];
+  _reload:boolean;
 
   public config: SwiperConfigInterface = {
     a11y: true,
@@ -62,14 +42,11 @@ export class TipsListComponent implements OnInit {
   constructor( private translateService: TranslateService ) { }
 
   ngOnInit() { 
-    // listen to the language change and get a new translation
     this.translateService.onDefaultLangChange.subscribe(event=>{
       const { content } = event.translations.homepage["eco-advices"];
-      const mutateData = this.addImagesToTips(content)
-      this.tips = mutateData;
-      //see console
-      console.log(this.tips)
-
+      this.tips = this.addImagesToTips(content);
+      setTimeout(()=>{this._reload = false})
+      setTimeout(()=>{this._reload = true})
     })
   }
   // simple add img path to each translate obj
